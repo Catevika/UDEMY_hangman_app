3fdf1e378b7905fb7129ec81e7cf69a5121f391b
Catevika <dominique.bello@outlook.fr>
2019-07-13 17:31:06 +0200

    Updates

---
+++ precache-manifest.4735bc8b45d20fd17ae970e15435e681.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11c979c867cc0c3e40f7f810b1a07cdc",
+    "url": "/UDEMY_hangman_app/index.html"
+  },
+  {
+    "revision": "c371fa1be6be83f884bf",
+    "url": "/UDEMY_hangman_app/static/css/main.92c41534.chunk.css"
+  },
+  {
+    "revision": "ded73dc3830fb2d80c34",
+    "url": "/UDEMY_hangman_app/static/js/2.b7dd99f8.chunk.js"
+  },
+  {
+    "revision": "c371fa1be6be83f884bf",
+    "url": "/UDEMY_hangman_app/static/js/main.4e77364e.chunk.js"
+  },
+  {
+    "revision": "6c2193574c0f4f38152c",
+    "url": "/UDEMY_hangman_app/static/js/runtime~main.269aedb3.js"
+  },
+  {
+    "revision": "db9097e874e68b7ed7115ff05ffcb880",
+    "url": "/UDEMY_hangman_app/static/media/0.db9097e8.jpg"
+  },
+  {
+    "revision": "fbff4784004cc97463c883477a0090d1",
+    "url": "/UDEMY_hangman_app/static/media/1.fbff4784.jpg"
+  },
+  {
+    "revision": "c4ea9528b67823bd94c90de3d97d75a3",
+    "url": "/UDEMY_hangman_app/static/media/2.c4ea9528.jpg"
+  },
+  {
+    "revision": "62a781c89c590abb27129638d9d18c4d",
+    "url": "/UDEMY_hangman_app/static/media/3.62a781c8.jpg"
+  },
+  {
+    "revision": "0f8ca81ee9f7b2bc27102267f9e23864",
+    "url": "/UDEMY_hangman_app/static/media/4.0f8ca81e.jpg"
+  },
+  {
+    "revision": "1b557aa7ccfa88b6b3f4723c27d3f2d6",
+    "url": "/UDEMY_hangman_app/static/media/5.1b557aa7.jpg"
+  },
+  {
+    "revision": "c80394c2a96ffad09a9df4e4815745f2",
+    "url": "/UDEMY_hangman_app/static/media/6.c80394c2.jpg"
+  }
+]);
